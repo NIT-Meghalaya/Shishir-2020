56cd8fcebd1c5c2aa08e1806416d8f4252dc969c
Dave <recd2dmax@gmail.com>
2020-02-28 20:41:55 +0530

    added dummy theme

---
+++ theme.js
@@ -0,0 +1,14 @@
+const theme = {
+  colors: {
+    primary: "#ffffff",
+    secondary: "#f3f3f3",
+    tertiary: "#e5e5e5",
+    accent: "#000000"
+  },
+  fonts: {
+    primary: "#f1f1f1",
+    secondary: "#000"
+  }
+};
+
+export default theme;
