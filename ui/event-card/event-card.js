a177788548916ed186aa51d35422a9b07d7acc90
Dave <recd2dmax@gmail.com>
2020-03-06 00:19:26 +0530

    removed redundant react import

--- ui/event-card/event-card.js
+++ ui/event-card/event-card.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import './event-card.styles.scss';
 
 const EventCard = ({ title, desc }) => {
--- ui/event-cards/event-cards.js
+++ ui/event-cards/event-cards.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import EventCard from '../event-card/event-card';
 
 const EventCards = ({ events }) => (
