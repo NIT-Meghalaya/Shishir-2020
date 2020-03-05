64592f2d218c39552e37ad59328f2e28ac93fc50
Dave <recd2dmax@gmail.com>
2020-03-06 00:03:23 +0530

    merge conflict errors

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import fetch from "isomorphic-unfetch";
 import "../public/styles/index.scss";
 import Header from '../ui/header/header';
 import SecondaryHeading from '../ui/secondary-heading/secondary-heading';
-import AboutUs from '../ui/about-us/about-us.component';
+import AboutUs from '../ui/about-us/about-us';
 import EventCards from '../ui/event-cards/event-cards';
 import absoluteUrl from './../lib/absolute_url';
 import '../public/events.json'
--- ui/event-cards/event-cards.js
+++ ui/event-cards/event-cards.js
@@ -1,33 +1,20 @@
 import React from 'react';
 import EventCard from '../event-card/event-card';
-import EVENT_DETAILS from './details';
 
-class EventCards extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      events:EVENT_DETAILS
-    }
-  }
-  
-  render() {
-    const { events } = this.state;
-    return (
-      <div className="container">
-        <section className="events-section">
-          {
-            events.forEach((event, index) => {
+const EventCards = ({ events }) => (
+  <div className="container">
+    <section className="events-section">
+      {
+        events.forEach((event, index) => {
           return (
-                <EventCard key={index+1} event={event}></EventCard>
-              )
-            })
-         }
-          
-        </section>
-        
-      </div>
-    )
-  }
-}
+            <EventCard key={index + 1} event={event}></EventCard>
+          )
+        })
+      }
+
+    </section>
+
+  </div>
+)
 
 export default EventCards;
