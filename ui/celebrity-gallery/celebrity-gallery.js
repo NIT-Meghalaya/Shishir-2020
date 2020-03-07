71d5c2da0adda4997fe65301161f10c92a8dc010
Abhushan <abhushanadhikarijoshi@gmail.com>
2020-03-08 00:46:29 +0530

    Added images for celebrities gallery + 1 missing

--- ui/celebrity-gallery/celebrity-gallery.js
+++ ui/celebrity-gallery/celebrity-gallery.js
@@ -11,26 +11,25 @@ const CelebrityGallery = () => {
             <div className="past-gallery">
                 <div className="gallery-top">
                     <figure>
-                        <img src="/images/music.jpg" />
-
+                        <img src="/images/celebrity/1.jpg" alt="Sreerama Chandra Mynampati" />
                     </figure>
                     <figure>
-                        <img src="/images/music.jpg" />
+                        <img src="/images/celebrity/2.jpg" alt="Nikhil D'Souza"/>
                     </figure>
                     <figure>
-                        <img src="/images/music.jpg" />
+                        <img src="/images/music.jpg" alt="Arsh Mohammad"/>
                     </figure>
                     
                 </div>
                 <div className="gallery-bottom">
                     <figure>
-                        <img src="/images/music.jpg" />
+                        <img src="/images/celebrity/4.jpg" alt="Gajendra Verma"/>
                     </figure>
                     <figure>
-                        <img src="/images/music.jpg" />
+                        <img src="/images/celebrity/5.jpg" alt="Kshitij Tarey"/>
                     </figure>
                     <figure>
-                        <img src="/images/music.jpg" />
+                        <img src="/images/celebrity/6.jpg" alt="Saurabhee Debbarma"/>
                     </figure>
                     
                 </div>
