36ba74d087775de998ad0c9324fd687b3e44213f
Dave <recd2dmax@gmail.com>
2020-03-08 16:18:55 +0530

    close navbar on link click

--- ui/nav/nav.js
+++ ui/nav/nav.js
@@ -26,31 +26,31 @@ export default class Nav extends React.Component {
             <div className="crosslines crosslines--1"></div>
             <div className="crosslines crosslines--2"></div>
           </button>
-         
+
           <div className="navigation">
             <div className="item">
               <Link href="/">
-                <a className="nav-link">Home</a>
+                <a className="nav-link" onClick={() => this.handleClick()}>Home</a>
               </Link>
             </div>
             <div className="item">
               <Link href="/events">
-                <a className="nav-link">Events</a>
+                <a className="nav-link" onClick={() => this.handleClick()}>Events</a>
               </Link>
             </div>
             <div className="item">
               <Link href="/teams">
-                <a className="nav-link">Teams</a>
+                <a className="nav-link" onClick={() => this.handleClick()}>Teams</a>
               </Link>
             </div>
             <div className="item">
               <Link href="/contact">
-                <a className="nav-link">Contact</a>
+                <a className="nav-link" onClick={() => this.handleClick()}>Contact</a>
               </Link>
             </div>
           </div>
         </div>
-        
+
       );
     else
       return (
