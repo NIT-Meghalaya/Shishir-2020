d0ac5480b6a251ceb74b8bbe375dac4d0d2724fe
Dave <recd2dmax@gmail.com>
2020-03-11 19:47:45 +0530

    removed title from document to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import App from "next/app";
+import Head from "next/head"
 import Nav from "../ui/nav/nav";
 
 export default class MyApp extends App {
@@ -6,6 +7,9 @@ export default class MyApp extends App {
     const { Component, pageProps } = this.props;
     return (
       <React.Fragment>
+        <Head>
+          <title>Shishir 2020</title>
+        </Head>
         <Nav />
         <Component {...pageProps} />
       </React.Fragment>
--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <title>Shishir 2020</title>
           <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" rel="stylesheet"></link>
         </Head>
         <body>
