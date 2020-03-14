feb2ae5b87b1b0aee31e9e62a21449b19f892c39
Dave <recd2dmax@gmail.com>
2020-03-14 19:13:11 +0530

    added description

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,7 @@ export default class MyApp extends App {
       <React.Fragment>
         <Head>
           <title>Shishir 2020</title>
+          <meta name="description" content="Shishir - NIT Meghalaya's Cultural Festival is back in 2020! Let's enjoy the festive atmosphere and sing and dance to our heart's content! Don't miss it!" />
         </Head>
         <Nav />
         <Component {...pageProps} />
