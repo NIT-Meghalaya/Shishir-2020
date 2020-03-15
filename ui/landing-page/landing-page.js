3d0e8cd251bd1956d2fac9f4ce4abb188ddfedb8
Dave <recd2dmax@gmail.com>
2020-03-15 13:30:11 +0530

    removed registration button

--- ui/landing-page/landing-page.js
+++ ui/landing-page/landing-page.js
@@ -6,18 +6,18 @@ const LandingPage = () => {
         <header className="herobox">
             <img src="/images/nit meghalaya.png" alt="NIT Meghlaya Logo" className="nitlogo" />
             <div className="herobox__text">
-                <img src="/images/shishir.png" alt="Shishir Logo" className="shishirlogo"/>
+                <img src="/images/shishir.png" alt="Shishir Logo" className="shishirlogo" />
                 <h1 className="primary-heading">Shishir'20</h1>
                 <p className="tagline">The Annual Cultural Fest of <span>NIT Meghalaya</span> </p>
                 <div className="herobox__socials">
-                    <a href="/" className="btn-primary">Explore</a>
-                    <a href="/registration" className="btn-primary">Register</a>
+                    <a href="#" className="btn-primary">Explore</a>
+                    {/* <a href="/registration" className="btn-primary">Register</a> */}
                 </div>
             </div>
             <div className="herobox__images">
-                <HeaderSvg/>
+                <HeaderSvg />
             </div>
-        
+
         </header>
     )
 }
